51206aec1184be1ab1152bcdaadc2a59e14068b0
Jardel Junior <55859324+jardeljj@users.noreply.github.com>
2025-03-28 21:16:06 -0300

    first commit, api de planetas

---
+++ src/main/java/com/example/sw_planet_api/domain/PlanetRepository.java
@@ -0,0 +1,9 @@
+package com.example.sw_planet_api.domain;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlanetRepository extends CrudRepository<Planet, Long>{
+
+}
---
+++ src/main/java/com/example/sw_planet_api/web/Controller/PlanetController.java
@@ -0,0 +1,31 @@
+package com.example.sw_planet_api.web.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.sw_planet_api.domain.Planet;
+import com.example.sw_planet_api.domain.PlanetService;
+
+@RestController
+@RequestMapping("/planets")
+public class PlanetController {
+    
+    @Autowired
+    private PlanetService planetService;
+
+
+    @PostMapping
+    public ResponseEntity<Planet> create(@RequestBody Planet planet){
+        Planet planetCreate = planetService.create(planet);
+        return ResponseEntity.status(HttpStatus.CREATED).body(planetCreate);
+    }
+
+    
+
+}
