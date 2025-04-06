9a5bf9de493b7948d479544dd2a9b6b1624b306c
Jardel Junior <55859324+jardeljj@users.noreply.github.com>
2025-04-05 21:29:09 -0300

    Implantado o cenario de sucesso com no controller.

---
+++ src/test/java/com/example/sw_planet_api/web/PlanetControllerTest.java
@@ -0,0 +1,43 @@
+package com.example.sw_planet_api.web;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+import com.example.sw_planet_api.domain.PlanetService;
+import com.example.sw_planet_api.web.Controller.PlanetController;
+import static com.example.common.PlanetConstants.PLANET;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(PlanetController.class)
+public class PlanetControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockitoBean
+    private PlanetService planetService;
+
+    @Test
+    public void createPlanet_WithValidData_returnCreated() throws Exception {
+        when(planetService.create(PLANET)).thenReturn(PLANET);
+
+        mockMvc.perform(post("/planets").content(objectMapper.writeValueAsString(PLANET))
+                        .contentType(MediaType.APPLICATION_JSON))
+                        .andExpect(status().isCreated())
+                        .andExpect(jsonPath("$").value(PLANET));
+    }
+
+}
