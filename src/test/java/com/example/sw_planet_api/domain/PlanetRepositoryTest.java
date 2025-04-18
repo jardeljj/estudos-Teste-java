2dedea8b78552833d7e20121a5860d201b95d860
Jardel Junior <55859324+jardeljj@users.noreply.github.com>
2025-04-18 01:44:09 -0300

    Aplicando testes parametrizados

--- src/test/java/com/example/sw_planet_api/domain/PlanetRepositoryTest.java
+++ src/test/java/com/example/sw_planet_api/domain/PlanetRepositoryTest.java
@@ -2,14 +2,20 @@ package com.example.sw_planet_api.domain;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Example;
 
+import net.bytebuddy.asm.Advice.Argument;
+
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -45,12 +51,29 @@ public class PlanetRepositoryTest {
         assertThat(sut.getTerrain()).isEqualTo(PLANET.getTerrain());
     }
 
-    @Test
-    public void createPlanet_WithInvalidData_ThrowsException(){
-        Planet emptyPlanet = new Planet();
-        Planet invalidPlanet =  new Planet("", "", "");
-        assertThatThrownBy(() -> planetRepository.save(emptyPlanet)).isInstanceOf(RuntimeException.class);
-        assertThatThrownBy(() -> planetRepository.save(invalidPlanet)).isInstanceOf(RuntimeException.class);
+    @ParameterizedTest
+    @MethodSource("providesInvalidPlanets")
+    public void createPlanet_WithInvalidData_ThrowsException(Planet planet){
+        
+        assertThatThrownBy(() -> planetRepository.save(planet)).isInstanceOf(RuntimeException.class);
+    }
+
+    private static Stream<Arguments> providesInvalidPlanets(){
+        return Stream.of(
+            Arguments.of(new Planet(null, "climate", "terrain")),
+            Arguments.of(new Planet("name", null, "terrain")),
+            Arguments.of(new Planet("name", "climate", null)),
+            Arguments.of(new Planet(null, null, "terrain")),
+            Arguments.of(new Planet(null, "climate", null)),
+            Arguments.of(new Planet("name", null, null)),
+            Arguments.of(new Planet(null, null, null)),
+            Arguments.of(new Planet("", "climate", "terrain")),
+            Arguments.of(new Planet("name", "", "terrain")),
+            Arguments.of(new Planet("name", "climate", "")),
+            Arguments.of(new Planet("", "", "terrain")),
+            Arguments.of(new Planet("", "climate", "")),
+            Arguments.of(new Planet("name", "", "")),
+            Arguments.of(new Planet("", "", "")));
     }
 
     @Test
